d0df22b641c9e6b1ea2343ff52f258acca6966c9
AritraStark <58631915+AritraStark@users.noreply.github.com>
2020-12-16 22:23:57 +0530

    3 working

--- A4Q3.cpp
+++ A4Q3.cpp
@@ -11,53 +11,68 @@ class ARRAY{
     // ARRAY(){
     //     arr = new int (0);
     // }
-    ARRAY(int n=0,int x=0){
-        arr = new int(n);
-        for(int i=0;i<n;i++)
-        arr[i]=x;
+    ARRAY(int s=0,int x=0){
+        if(s>0)
+		{
+			n=s;
+			arr=new int[n];
+			for(int i=0;i<n;i++)
+				arr[i]=x;
+		}
+		else{
+			n=0;
+			arr=NULL;
+		}
     }
     ARRAY(const ARRAY &a){
-        // n=a.n;
-        // arr = new int(n);
+        n=a.n;
+        arr = new int(n);
         for(int i=0;i<a.n;i++)
         arr[i] = a.arr[i];
     }
-    ARRAY(int arr1[],int n1){
-        n = n1;
-        //arr = new int(n1);
-        for(int i=0;i<n+3;i++)
-        arr[i]=arr1[i];
-        //arr=arr1;
+    ARRAY(int *arr1){
+        arr = arr1;
     }
-    ARRAY operator+(const ARRAY &c){
-        ARRAY a1(c.n+5,0);
-        for(int i =0 ; i<c.n+5;i++)
+
+    void operator= (const int* p)
+	{	
+		for(int i=0;i<n;i++)
+			arr[i]=p[i];
+	}
+
+	void operator= (const ARRAY &a)
+	{
+
+		n=a.n;
+		arr=new int[a.n];
+		for(int i=0;i<n;i++)
+			arr[i]=a.arr[i];
+	}
+
+
+    ARRAY operator+ (const ARRAY &c){
+        ARRAY a1(c.n,0);
+        for(int i =0 ; i<c.n;i++)
         a1.arr[i] = arr[i] + c.arr[i];
         return a1;
     }
-    // ARRAY operator= (ARRAY const &c){
-    //     for(int i =0;i<c.n;i++)
-    //     arr[i] = c.arr[i];
-    // }
+
     int& operator[](int i){
-        // if (i > n) { 
-        //     ; 
-        // } 
         return arr[i]; 
     }
-    friend ARRAY operator*(ARRAY , int );
+
+    ARRAY operator *(int x)
+	{
+		ARRAY c(n);
+		for(int i=0;i<n;i++)
+			c.arr[i]=x*arr[i];
+		return c;
+	}
 };
 
-ARRAY operator* (ARRAY a, int x){
-        for(int i =0;i<a.n;i++)
-        a.arr[i]*=x;
-    }
 
 int main(){
-    ARRAY a(6,10);
-    for(int i=0;i<6;i++)
-    cout<<a[i]<<" ";
-    cout<<endl;
+
     
     ARRAY b(6,5);
     for(int i=0;i<6;i++)
@@ -65,30 +80,147 @@ int main(){
     cout<<endl;
 
     
-    ARRAY c(6);
-    c = b;
-    for(int i=0;i<6;i++)
-    cout<<c[i]<<" ";
-    cout<<endl;
+
     
-    ARRAY d = b+a;
-    for(int i=0;i<6;i++)
-    cout<<d[i]<<" ";
-    cout<<endl;
+
     
-    ARRAY f(6);
-    f = a*5;
-    for(int i=0;i<6;i++)
-    cout<<a[i]<<" ";
-    cout<<endl;
     
-    int x[6];
-    for(int i=0;i<6;i++)
-    x[i] = 4;
-    ARRAY e = ARRAY(x,10);
-    for(int i=0;i<6;i++)
-    cout<<e[i]<<" ";
-    cout<<endl;
     
+    
+    
+
+    for(;;){
+        cout<<"Enter : \n1. To create an Array object\n2. To create an Array object with another Array object\n3. To create an Array object with a primitive array\n4. To add two Array objects\n5. To multiply Array object with an integer\n6. To equate two Array objects\n7. To display all elements of an Array object using the subscript operator\n8. To exit";
+        int c;
+        cin>>c;
+        switch(c){
+            case 1:{
+                int a,b;
+                cout<<"Enter size and value for assignment\n";
+                cin>>a>>b;
+                ARRAY a1(a,b);
+                cout<<"The Array is: \n";
+                for(int i=0;i<a;i++)
+                cout<<a1[i]<<" ";
+                cout<<endl;
+                break;
+            }
+            case 2:{
+                int a,b;
+                cout<<"Enter size and value for assignment of first Array object\n";
+                cin>>a>>b;
+                ARRAY b1(a,b);
+                cout<<"The first Array object is: \n";
+                for(int i=0;i<a;i++)
+                cout<<b1[i]<<" ";
+                cout<<endl;
+                cout<<"The second copied Array object is: \n";           
+                ARRAY c(a);
+                c = b;
+                for(int i=0;i<a;i++)
+                cout<<c[i]<<" ";
+                cout<<endl;
+                break;
+            }
+            case 3:{
+                int a,b;
+                cout<<"Enter size and value for assignment of primitive array\n";
+                cin>>a>>b;
+                int *x;
+                x = new int(a); 
+                for(int i=0;i<a;i++)
+                x[i] = b;
+                cout<<"The primitive array is: \n";
+                for(int i=0;i<a;i++)
+                cout<<x[i];
+                cout<<endl;
+                ARRAY e(x);
+                cout<<"The result is: \n";
+                for(int i=0;i<a;i++)
+                cout<<e[i]<<" ";
+                cout<<endl;
+                break;
+            }
+            case 4:{
+                int a,b;
+                cout<<"Enter size and value for assignment of first Array object\n";
+                cin>>a>>b;
+                ARRAY b1(a,b);
+                cout<<"The first Array object is: \n";
+                for(int i=0;i<a;i++)
+                cout<<b1[i]<<" ";
+                int x,y;
+                cout<<"Enter size and value for assignment of second Array object\n";
+                cin>>x>>y;
+                ARRAY b2(x,y);
+                cout<<"The second Array object is: \n";
+                for(int i=0;i<a;i++)
+                cout<<b2[i]<<" ";
+                if(x!=a){
+                    cout<<"Array object lengths are not same, please enter same length\n";
+                    break;
+                }
+                ARRAY d = b1+b2;
+                cout<<"The added array objects result is:\n";
+                for(int i=0;i<a;i++)
+                cout<<d[i]<<" ";
+                cout<<endl;
+                break;
+            }
+            case 5:{
+                int a,b,c;
+                cout<<"Enter size and value for assignment of first Array object\n";
+                cin>>a>>b;
+                ARRAY b1(a,b);
+                cout<<"The first Array object is: \n";
+                for(int i=0;i<a;i++)
+                cout<<b1[i]<<" ";
+                cout<<"Enter integer to multiply object with:\n";
+                cin>>c;
+                ARRAY f(6);
+                f = b1*c;
+                cout<<"The result is: \n";
+                for(int i=0;i<a;i++)
+                cout<<f[i]<<" ";
+                cout<<endl;
+                break;
+            }
+            case 6:{
+                int a,b;
+                cout<<"Enter size and value for assignment of first Array object\n";
+                cin>>a>>b;
+                ARRAY b1(a,b);
+                cout<<"The first Array object is: \n";
+                for(int i=0;i<a;i++)
+                cout<<b1[i]<<" ";
+                cout<<"The equated Array object is\n";
+                ARRAY c(a);
+                c = b1;
+                for(int i=0;i<a;i++)
+                cout<<c[i]<<" ";
+                cout<<endl;
+                break;
+            }
+            case 7:{
+                int a,b;
+                cout<<"Enter size and value for assignment of first Array object\n";
+                cin>>a>>b;
+                ARRAY b1(a,b);
+                cout<<"The Array object is: \n";
+                for(int i=0;i<a;i++)
+                cout<<b1[i]<<" ";
+                break;
+            }
+            case 8:{
+                cout<<"Exiting ... \n";
+                exit(0);
+                break;
+            }
+            default:{
+                cout<<"Wrong input";
+                break;
+            }
+        }
+    }
     return 0;
 }
---
+++ A4Q6.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Student{
+    int roll,phNo,sbCt;
+    string name;
+    Student* next;
+};
+
+class Subject{
+    int code,stCt;
+    string name;
+    Subject* next;
+};
+
+
+
+int main(){
+
+    return 0;
+}
